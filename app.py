72865c3266ccec810a7991f3e024e69654d97dbd
6lueparr0t <6lueparr0t@gmail.com>
2025-01-05 15:19:51 +0900

    feat: Gemini API Test

--- app.py
+++ app.py
@@ -1,34 +1,31 @@
 import os
 from dotenv import load_dotenv
-from openai import OpenAI
+import google.generativeai as genai
 
 # (1) .env 파일 로드
 load_dotenv()  # 기본적으로 현재 프로젝트 루트/실행 위치의 .env를 탐색합니다.
 
 # (2) 환경변수에서 키 가져오기
-api_key = os.environ.get("OPENAI_API_KEY")
+api_key = os.environ.get("GEMINI_API_KEY")
 
-# (3) OpenAI 클라이언트 생성
-client = OpenAI(api_key=api_key)
 
 def main():
-    try:
-        # (4) ChatCompletion API 호출
-        chat_completion = client.chat.completions.create(
-            messages=[
-                {
-                    "role": "user",
-                    "content": "Say this is a test",
-                }
-            ],
-            model="gpt-4o-mini",
-        )
-        # 결과 출력
-        print(chat_completion)
-    
-    except Exception as e:
-        print("알 수 없는 오류 발생:", e)
+  try:
+      genai.configure(api_key=api_key)
+      model = genai.GenerativeModel("gemini-1.5-flash")
+      chat = model.start_chat(history=[])
+
+      while True:
+          message = input("Q)")
+          if (message == "exit"):
+              break
+          response = chat.send_message(message)
+          print("A)")
+          print(response.text)
+
+  except Exception as e:
+      print("알 수 없는 오류 발생:", e)
+
 
 if __name__ == "__main__":
     main()
-

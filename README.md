3f8a9d5a6c0abfafff98a13135817cede0a5398a
6lueparr0t <6lueparr0t@gmail.com>
2025-03-05 18:12:46 +0900

    feat: start gemini api

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# python-llm-collection
+# Python LLM Collection
+
+이 저장소는 Python 기반 LLM(대형 언어 모델) 실행 환경을 구축하고 활용하는 방법을 제공합니다.
+
+## 환경 설정
+
+1. Conda 환경을 활성화
+
+```sh
+conda activate llm
+```
+
+2. Jupyter Notebook 실행
+
+```sh
+jupyter notebook
+```
--- app.py
+++ app.py
@@ -12,7 +12,7 @@ api_key = os.environ.get("GEMINI_API_KEY")
 def main():
     try:
         genai.configure(api_key=api_key)
-        model = genai.GenerativeModel("gemini-1.5-flash")
+        model = genai.GenerativeModel("gemini-2.0-flash-lite")
         chat = model.start_chat(history=[])
 
         while True:
